6c846715a2dc48c8cf9116c25679dc39fc37ac00
Elnar Zulfuqarli <elnarzulfuqarli2001@gmail.com>
2023-11-24 16:21:45 +0400

    Select palan and Setup guide

--- app.js
+++ app.js
@@ -17,3 +17,32 @@ document.addEventListener('DOMContentLoaded', function () {
         dropdownElement.classList.remove('active-dropdown');
     });
 });
+
+    // Setup Guide Click
+document.addEventListener('DOMContentLoaded', function () {
+    const setupTitle = document.querySelector('.setup-title');
+    const customizes = document.querySelector('.customizes');
+    const angleIcon = document.querySelector('.setup-title i');
+
+    setupTitle.addEventListener('click', function () {
+        customizes.style.display = customizes.style.display === 'none' ? 'flex' : 'none'; 
+        angleIcon.style.transform = customizes.style.display === 'none' ? 'rotate(0deg)' : 'rotate(180deg)';
+    });
+
+    const customizeHeads = document.querySelectorAll('.customize-head');
+
+    customizeHeads.forEach(function (customizeHead) {
+        customizeHead.addEventListener('click', function () {
+            const customizeTheme = this.nextElementSibling;
+            const otherThemes = document.querySelectorAll('.customize-theme');
+
+            otherThemes.forEach(function (otherTheme) {
+                if (otherTheme !== customizeTheme) {
+                    otherTheme.style.display = 'none';
+                }
+            });
+            
+            customizeTheme.style.display = customizeTheme.style.display === 'none' ? 'flex' : 'none'; 
+        });
+    });
+});
