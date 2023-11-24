ea906eecd1880bdac901a857f895bc98cc38f752
Elnar Zulfuqarli <elnarzulfuqarli2001@gmail.com>
2023-11-24 10:41:30 +0400

    Profile and Notification Dropdown Add and fixed.

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const profileElement = document.querySelector('.navbar-profile');
+    const notificationIcon = document.getElementById('notificationIcon');
+    
+    const dropdownElement = document.querySelector('.navbar-profile-dropdown');
+    const notificationAlert = document.getElementById('notificationAlert');
+
+    // Profile Dropdown Click
+    profileElement.addEventListener('click', () => {
+        dropdownElement.classList.toggle('active-dropdown');
+        notificationAlert.classList.remove('active');
+    });
+
     // Notification Alert click
-document.getElementById('notificationIcon').addEventListener('click', function() {
-    document.getElementById('notificationAlert').classList.toggle('active');
+    notificationIcon.addEventListener('click', function() {
+        notificationAlert.classList.toggle('active');
+        dropdownElement.classList.remove('active-dropdown');
+    });
 });
